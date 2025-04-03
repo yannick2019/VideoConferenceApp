d61c0043dc8ac8afc5f6eb8574f0aaf44ebc1790
Yannick <cleps2012@gmail.com>
2025-04-03 12:18:22 +0200

    Create authentication state provider and frontend services

---
+++ VideoConferenceApp.Client/Services/IAuthService.cs
@@ -0,0 +1,26 @@
+﻿using VideoConferenceApp.Shared.Authentication.Requests;
+using VideoConferenceApp.Shared.Authentication.Responses;
+
+namespace VideoConferenceApp.Client.Services
+{
+    public interface IAuthService
+    {
+        Task<LoginUserResponse?> Login(LoginUserRequest user);
+        Task<CreateUserResponse?> CreateUserAccount(CreateUserRequest user);
+    }
+
+    public class AuthService(IHttpExtension httpExtension) : IAuthService
+    {
+        public async Task<CreateUserResponse?> CreateUserAccount(CreateUserRequest user)
+        {
+            var result = await httpExtension.GetPublicClient().PostAsJsonAsync("auth/create", user);
+            return await result.Content.ReadFromJsonAsync<CreateUserResponse>();
+        }
+
+        public async Task<LoginUserResponse?> Login(LoginUserRequest user)
+        {
+            var result = await httpExtension.GetPublicClient().PostAsJsonAsync("auth/login", user);
+            return await result.Content.ReadFromJsonAsync<LoginUserResponse>();
+        }
+    }
+}
---
+++ VideoConferenceApp.Client/States/CustomAuthStateProvider.cs
@@ -0,0 +1,69 @@
+﻿using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using Microsoft.AspNetCore.Components.Authorization;
+using NetcodeHub.Packages.Extensions.LocalStorage;
+
+namespace VideoConferenceApp.Client.States
+{
+    public class CustomAuthStateProvide
+        (ILocalStorageService localStorageService, IConfiguration configuration) : AuthenticationStateProvider
+    {
+        ClaimsPrincipal User = new(new ClaimsIdentity());
+
+        public async override Task<AuthenticationState> GetAuthenticationStateAsync()
+        {
+            // Get token key
+            string key = configuration["Token:Key"]!;
+            if (key == null)
+            {
+                return await Task.FromResult(new AuthenticationState(User));
+            }
+
+            // Get token from local storage
+            string token = await localStorageService.GetItemAsStringAsync(key);
+            if (token == null)
+            {
+                return await Task.FromResult(new AuthenticationState(User));
+            }
+
+            User = SetClaim(token);
+
+            return await Task.FromResult(new AuthenticationState(User));
+        }
+
+        public async Task SetUserAuthenticated(string token)
+        {
+            if (configuration["Token:Key"] == null) return;
+
+            string key = configuration["Token:Key"]!;
+            await localStorageService.SaveAsStringAsync(key, token);
+            User = SetClaim(key);
+            NotifyAuthenticationStateChanged(Task.FromResult(new AuthenticationState(User)));
+        }
+
+        public async Task SetUserLoggedOut()
+        {
+            string key = configuration["Token:Key"]!;
+            if (key == null) return;
+
+            await localStorageService.DeleteItemAsync(key);
+            User = new ClaimsPrincipal(new ClaimsIdentity());
+            NotifyAuthenticationStateChanged(Task.FromResult(new AuthenticationState(User)));
+        }
+
+        private static ClaimsPrincipal SetClaim(string token)
+        {
+            try 
+            {
+                var tokenHandler = new JwtSecurityTokenHandler();
+                var jwtToken = tokenHandler.ReadJwtToken(token);
+                var claims = jwtToken.Claims;
+                return new ClaimsPrincipal(new ClaimsIdentity(claims, "JwtAuth"));
+            }
+            catch
+            {
+                return new(new ClaimsIdentity());
+            }
+        }
+    }
+}
