043fa6abc2fb160b88863f638d67eb4fd9ddac96
Yannick <cleps2012@gmail.com>
2025-04-08 12:08:48 +0200

    Create meeting and twilio services

---
+++ VideoConferenceApp.Client/Services/IMeetingService.cs
@@ -0,0 +1,35 @@
+﻿using System.Net.Http.Json;
+using VideoConferenceApp.Client.Extensions;
+using VideoConferenceApp.Shared.Meeting.Requests;
+using VideoConferenceApp.Shared.Meeting.Responses;
+
+namespace VideoConferenceApp.Client.Services
+{
+    public interface IMeetingService
+    {
+        Task<CreateMeetingResponse?> CreateMeeting(CreateMeetingRequest meeting);
+        Task<GetMeetingsResponse?> GetMeetings(string hostId);
+        Task<GetRecentMeetingsResponse?> GetRecentMeetings(string hostId);
+    }
+
+    public class MeetingService(IHttpExtension httpExtension) : IMeetingService
+    {
+        public async Task<CreateMeetingResponse?> CreateMeeting(CreateMeetingRequest meeting)
+        {
+            var result = await (await httpExtension.GetPrivateClient()).PostAsJsonAsync("meeting/create", meeting);
+            return await result.Content.ReadFromJsonAsync<CreateMeetingResponse>();
+        }
+
+        public async Task<GetMeetingsResponse?> GetMeetings(string hostId)
+        {
+            var result = await (await httpExtension.GetPrivateClient()).GetAsync($"meeting/{hostId}");
+            return await result.Content.ReadFromJsonAsync<GetMeetingsResponse>();
+        }
+
+        public async Task<GetRecentMeetingsResponse?> GetRecentMeetings(string hostId)
+        {
+            var result = await (await httpExtension.GetPrivateClient()).GetAsync($"meeting/recent/{hostId}");
+            return await result.Content.ReadFromJsonAsync<GetRecentMeetingsResponse>();
+        }
+    }
+}
---
+++ VideoConferenceApp.Client/Services/ITwilioService.cs
@@ -0,0 +1,27 @@
+﻿using System.Net.Http.Json;
+using Microsoft.JSInterop;
+using VideoConferenceApp.Client.Extensions;
+using VideoConferenceApp.Shared.Meeting.Responses;
+
+namespace VideoConferenceApp.Client.Services
+{
+    public interface ITwilioService
+    {
+        Task<TwilioServiceResponse?> GenerateMeetingToken(string username, string meetingId);
+        Task JoinMeeting(string token, string roomName, string containerId);
+    }
+
+    public class TwilioService(IJSRuntime _js, IHttpExtension httpExtension) : ITwilioService
+    {
+        public async Task<TwilioServiceResponse?> GenerateMeetingToken(string username, string meetingId)
+        {
+        var result = await httpExtension.GetPublicClient().GetAsync($"twilio/token/{username}/{meetingId}");
+        return await result.Content.ReadFromJsonAsync<TwilioServiceResponse>();
+        }
+
+        public async Task JoinMeeting(string token, string roomName, string containerId)
+        {
+            await _js.InvokeVoidAsync("window.twilioVideo.connectToRoom", token, roomName, containerId);
+        }
+    }
+}
